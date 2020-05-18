db5c76c03c9fe06997f96a32507f5cccf9e6551b
Brian Levin <bml201095@gmail.com>
2020-05-17 23:32:39 -0400

    commeneted out some of my code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer'); // inquirer library to run the prompts
 
-const mysql = require("mysql");
+const mysql = require("mysql"); // my sql library to run  mysql 
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({ // the connecttion which connect to mysql
     host: "127.0.0.1",
 
     user: "Brian",
@@ -12,14 +12,14 @@ const connection = mysql.createConnection({
     database: "employees_db"
 });
 
-connection.connect(function (err) {
+connection.connect(function (err) { // error if it doesnt work
     if (err) {
         throw err;
     }
 })
 
-const questions = [{
-    type: 'list', // This will display the list of licenses
+const questions = [{ // list the user what they would like to add
+    type: 'list',
     name: 'options',
     message: 'What would you like to do?',
     choices: ['add employee', 'add role', 'add department', 'view employee', 'view role', 'view department', 'update employee roles', 'None'], // the choices for the differant license types
@@ -27,7 +27,7 @@ const questions = [{
 
 }];
 
-inquirer.prompt(questions).then(function (answers) {
+inquirer.prompt(questions).then(function (answers) { //if else statements for the choices that the user picked in the command line
     if (answers.options === 'add department') {
         addDepartment();
     } else if (answers.options === 'add employee') {
@@ -38,7 +38,7 @@ inquirer.prompt(questions).then(function (answers) {
 
 });
 
-function addDepartment() {
+function addDepartment() { // function whcih will add department
     inquirer.prompt([{
         type: 'input',
         name: 'departmentName',
@@ -54,14 +54,14 @@ function addDepartment() {
 
             function (err, res) {
                 if (err) throw err;
-                console.log(res.affectedRows + " product inserted!\n");
+                console.log(res.affectedRows + " product inserted!\n"); // the department will be inserted
                 // Call updateProduct AFTER the INSERT completes
 
             }
         );
     })
 }
-function addEmployee() {
+function addEmployee() { // function which will add employee
     inquirer.prompt([{
         type: 'input',
         name: 'employeeName',
@@ -77,15 +77,15 @@ function addEmployee() {
             },
             function (err, res) {
                 if (err) throw err;
-                console.log(res.affectedRows + " product inserted!\n");
-                // Call updateProduct AFTER the INSERT completes
+                console.log(res.affectedRows + " product inserted!\n"); // the new employee will be inserted
+
 
             }
         );
     })
 
 }
-function addRole() {
+function addRole() { // function which will add the role
     inquirer.prompt([{
         type: 'input',
         name: 'employeeRole',
@@ -101,8 +101,8 @@ function addRole() {
             },
             function (err, res) {
                 if (err) throw err;
-                console.log(res.affectedRows + " product inserted!\n");
-                // Call updateProduct AFTER the INSERT completes
+                console.log(res.affectedRows + " product inserted!\n"); // new role will be inserted
+
 
             }
         );
@@ -110,7 +110,7 @@ function addRole() {
 
 }
 
-function updateEmployeeRole() {
+function updateEmployeeRole() { //function which will update the employeeRole
     inquirer.prompt([{
         type: 'input',
         name: 'updateRole',
@@ -119,12 +119,12 @@ function updateEmployeeRole() {
     }]).then(function (answers) {
         console.log(answers);
         var query = connection.query(
-            "UPDATE statement ? , employee SET role_id= 2 WHERE id= 42;",
+            "UPDATE statement ? , employee SET role_id= 2 WHERE id= 42;", // put the update staement into the connection
 
             function (err, res) {
                 if (err) throw err;
-                console.log(res.affectedRows + " product inserted!\n");
-                // Call updateProduct AFTER the INSERT completes
+                console.log(res.affectedRows + " product inserted!\n"); // the updated employee will then be inserted
+
 
             }
         );
