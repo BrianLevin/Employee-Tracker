d86c2a9c0087fb201e32831982ae6c6b23895132
Brian Levin <bml201095@gmail.com>
2020-05-17 23:38:37 -0400

    made some comments in my tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `department`(
+CREATE TABLE `department`( --department table and all its values
 id INT NOT NULL,
 make VARCHAR(30),
 PRIMARY KEY(id)
 );
 
-CREATE TABLE `role` (
+CREATE TABLE `role` ( -- role table and all its values
 id INT NOT NULL,
 make VARCHAR(30),
 salary DECIMAL,
@@ -12,7 +12,7 @@ salary DECIMAL,
 
 );
 
-CREATE TABLE `employee` (
+CREATE TABLE `employee` ( -- employee table and all its values
 id INT NOT NULL
 first_name VARCHAR(30),
 last_name VARCHAR(30),
@@ -22,13 +22,13 @@ manager_id INT
 
 );
 
-SELECT employee.id, employee.first_name, employee.last_name,
+SELECT employee.id, employee.first_name, employee.last_name, -- Select interface which will allow me to view all the tables
 	role.title, role.salary, department.name AS departmentName
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.department_id = department.id;
 
-UPDATE `employee`
+UPDATE `employee` --UPDATE Function whcih will aloow to update the employees role
 
 SET
 role_id=2 
