a9ef2b33e687a0179f6d3f2afa1f830dc3d5bec8
Brian Levin <bml201095@gmail.com>
2020-08-05 01:07:16 -0400

    added images

--- db/seed.sql
+++ db/seed.sql
@@ -20,7 +20,7 @@ VALUES
     ('Legal Team Lead', 250000, 4),
     ('Lawyer', 190000, 4);
 
-INSERT INTO employee_db
+INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
